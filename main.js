d69352fc567b5e17e78a80dfb2228d1810d36c06
dv-kong <70198643+dv-kong@users.noreply.github.com>
2021-09-08 14:52:12 +0200

    Lesson 2 done

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description: "A pair soft and warm socks for everyday use.",
+    };
+  },
+});
