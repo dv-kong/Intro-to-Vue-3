7010b794eacc3955d36bf14090eab0926f77655f
dv-kong <70198643+dv-kong@users.noreply.github.com>
2021-09-08 18:18:17 +0200

    l6 wip

--- main.js
+++ main.js
@@ -12,10 +12,12 @@ const app = Vue.createApp({
       ],
       methods: {
         addToCart() {
-          this.cart += 1
+          this.cart += 1;
         },
         removeFromCart() {
-          this.cart -= 1
+          if (cart >= 1) {
+            this.cart -= 1;
+          }
         },
         updateImage(variantImage) {
           this.image = variantImage;
